1bd27f3923e1b1cab9e6a2f7b6bf33e890d440d6
Smachy <pro.diegocdossantos@gmail.com>
2023-02-01 11:29:45 +0100

    roles in config

--- src/init.php
+++ src/init.php
@@ -3,4 +3,12 @@
 require_once __DIR__ . '/config.example.php';
 require_once __DIR__ . '/db.php';
 
+$roles = [
+    "admin"=>1000,
+    "manager"=>200,
+    "verified_user"=>10,
+    "unverified_user"=>1,
+    "banned_user"=>0
+];
+
 ?>
