c525148b8a604aa5ccd079fd3d422f78516dc9b1
mathis <mathis.renaudin95@gmail.com>
2023-02-02 11:52:28 +0100

    html/css page convertion finish

--- src/pages/home.php
+++ src/pages/home.php
@@ -1,6 +1,12 @@
-<H1>Acceuil</H1>
+<div id="real_body">
+                <h1>La nouvelle banque révolutionnaire !</h1>
+                <button>Disponible prochainement...</button>
+        </div>
 
     <?php 
     if ($user) { ?>
+
+        
         <h3> Bonjour <?php echo $user['fullname']; ?> ! </h3>
+        
     <?php } ?>
