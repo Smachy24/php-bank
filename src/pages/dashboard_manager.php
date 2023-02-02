9afb7f131cd9c86032cd0ff3d2de573abb961425
Tartuffe-coding <khaled.kouadria@edu.esiee-it.fr>
2023-02-02 14:08:14 +0100

    creating dashboard for manager and pending deposit and withdraw table generation

--- src/pages/dashboard_manager.php
+++ src/pages/dashboard_manager.php
@@ -15,52 +15,8 @@
 <h1>Pannel Manager</h1>
 <h3> Bonjour Manager <?php echo $user['fullname']; ?> ! </h3>
 
+    <?php gen_table_structure("deposit") ?>
+    <br>
+    <?php //gen_table_structure("withdrawal") ?>
 
-<!-- TABLEAU DES Deposit -->
-
-<section id="score_table_section">
-        <!-- formulaire pour gerer le sorting -->
-        <form action="/index.php?page=dashboard_manager" method="POST">  
-
-            <div class="form_section">
-                <label>sorting</label>
-                <select name="sorting">
-                    <option value="date">date</option>
-                    <option value="montant">montant</option>
-                    <option value="fullname">nom</option>
-                    <option value="currency">currency</option>
-                </select>
-            </div>
-
-            <div>
-                <input type="submit" id="valider" value="Valider" name="sorting_submit">
-            </div>
-        </form>
-
-        <!-- formulaire pour etre en mode vision des scores personnel uniquement -->
-
-        <table>
-            <thead>
-                <tr>
-                    <th>Utilisateur</th>
-                    <th>Monaie</th>
-                    <th>Montant</th>
-                    <th>Date demande</th>
-                </tr>
-            </thead>
-
-            <!-- pour chaque ligne de la tale cible, on va l'affecter a la variable $row et lui appliquer un traitement pour qu'elle affiche (echo) les donnée de colones dans différent balises <th> -->
-            <tbody>
-                <?php
-                $sorting = "date";
-                if (isset($_POST['sorting'])) {
-                    $sorting = $_POST['sorting'];
-                }
-
-                genScores('deposit', $sorting);
-
-                ?>
-            </tbody>
-        </table>
-    </section>
 
