0e802d251d119887fd99632856e92f33b0a8ba44
Tartuffe-coding <khaled.kouadria@edu.esiee-it.fr>
2023-02-03 09:37:17 +0100

    update dynamic nav for not displaying login and registered when connected

--- src/partials/nav.php
+++ src/partials/nav.php
@@ -27,26 +27,33 @@
                     if ($user)
                     {
                         if ($user['role'] == 1000) { ?>
+                            <!-- page s'affichant si connecté et admin -->
                             <button><a class="menu_a" href="/index.php?page=dashboard_admin">admin</a></button>
 
                         <?php }
                         if ($user['role'] >= 200) { ?>
+                            <!-- page s'affichant si connecté et manager -->
                             <button><a class="menu_a" href="/index.php?page=dashboard_manager">Manager</a></button>
                         <?php }
                         if ($user['role'] >= 10) { ?>
+                            <!-- page s'affichant si connecté et vérifié -->
                             <button><a class="menu_a" href="/index.php?page=deposit">Deposer</a></button>
                             <button><a class="menu_a" href="/index.php?page=withdrawal">Retrait</a></button>
                             <button><a class="menu_a" href="/index.php?page=convertion">Convertion</a></button>
                             <button><a class="menu_a" href="/index.php?page=transfer">Virement</a></button>
-                        <?php } 
-                        ?><button><a class="menu_a" href="/index.php?page=account">Compte</a></button>
-                          <button><a class="menu_a" href="/actions/logout.php">Logout</a></button><?php
+                        <?php } ?> 
+                            <!-- page s'affichant dans toute situation si connecté -->
+                            <button><a class="menu_a" href="/index.php?page=account">Compte</a></button>
+                            <button><a class="menu_a" href="/actions/logout.php">Logout</a></button><?php
 
-                    }?>
-                    
+                    }
+                    else { ?>
+                        <!-- page s'affichant si déconnecté -->
+                        <button><a class="menu_a" href="/index.php?page=register">Inscription</a></button>
+                        <button><a class="menu_a" href="/index.php?page=login">Connexion</a></button>
+                    <?php } ?>
+                    <!-- page s'affichant dans toute situation -->
                     <button><a class="menu_a" href="/index.php?page=home">Acceuil</a></button>
-                    <button><a class="menu_a" href="/index.php?page=register">Inscription</a></button>
-                    <button><a class="menu_a" href="/index.php?page=login">Connexion</a></button>
                             
                     
                     
